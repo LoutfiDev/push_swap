0e129f81c9654dc832a4b13bea159764d9e0405c
Yahya Loutfi <yloutfi@e2r7p1.1337.ma>
2023-01-05 15:53:22 +0100

    makefile added

--- Makefile
+++ Makefile
@@ -6,17 +6,17 @@
 #    By: yloutfi <yloutfi@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/01/03 08:43:01 by yloutfi           #+#    #+#              #
-#    Updated: 2023/01/03 08:47:27 by yloutfi          ###   ########.fr        #
+#    Updated: 2023/01/05 15:47:40 by yloutfi          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-SRCS = 
+SRCS = push_swap.c push_swap_utils.c stack_utils.c
 
-OBJS = 
+OBJS = push_swap.o push_swap_utils.o stack_utils.o
 
 BONUS_SRCS = 				
 
-BONUS_OBJS = 			
+BONUS_OBJS =		
 	
 NAME = push_swap.a
 CFLAGS = -Wall -Wextra -Werror
@@ -25,10 +25,12 @@ CC = cc
 all : $(NAME)
 
 bonus : $(BONUS_OBJS)
-	ar rc $(NAME) $(BONUS_OBJS) -o checker
+	$(CC) $(CFLAGS) $(BONUS_SRCS) -o checker
+	ar rc $(NAME) $(BONUS_OBJS)
 	
 $(NAME) : $(OBJS)
-	ar rc $(NAME) $(OBJS) -o push_swap
+	$(CC) $(CFLAGS) $(SRCS) -o push_swap
+	ar rc $(NAME) $(OBJS)
 
 clean :
 	-rm -f $(OBJS) $(BONUS_OBJS)
--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: yloutfi <yloutfi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 08:42:26 by yloutfi           #+#    #+#             */
-/*   Updated: 2023/01/05 14:55:42 by yloutfi          ###   ########.fr       */
+/*   Updated: 2023/01/05 15:52:55 by yloutfi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,35 @@
 
 int	main(int ac, char **av)
 {	
-	t_stack *head_a = NULL;
-	t_stack *second_a = NULL;
-	t_stack *third_a = NULL;
-	
-	t_stack *head_b = NULL;
-	t_stack *second_b = NULL;
-	t_stack *third_b = NULL;
-	
-	head_a = ft_lstnew(10);
-	second_a = ft_lstnew(20);
-	third_a = ft_lstnew(30);
-	
-	head_b = ft_lstnew(40);
-	second_b = ft_lstnew(50);
-	third_b = ft_lstnew(60);
-	
-	head_a->next = second_a;
-	second_a->next = third_a;
-	third_a->next = NULL;
-	
-	head_b->next = second_b;
-	second_b->next = third_b;
-	third_b->next = NULL;
-	
-	rev_rotate(&head_b);
-	ft_lstprint(head_b);
+
 	return (0);
 }
+
+	// t_stack *head_a = NULL;
+	// t_stack *second_a = NULL;
+	// t_stack *third_a = NULL;
+	
+	// t_stack *head_b = NULL;
+	// t_stack *second_b = NULL;
+	// t_stack *third_b = NULL;
+	
+	// head_a = ft_lstnew(10);
+	// second_a = ft_lstnew(20);
+	// third_a = ft_lstnew(30);
+	
+	// head_b = ft_lstnew(40);
+	// second_b = ft_lstnew(50);
+	// third_b = ft_lstnew(60);
+	
+	// head_a->next = second_a;
+	// second_a->next = third_a;
+	// third_a->next = NULL;
+	
+	// head_b->next = second_b;
+	// second_b->next = third_b;
+	// third_b->next = NULL;
+	
+	// rev_rotate(&head_b);
+	// ft_lstprint(head_b);
+	
+	// system("leaks push_swap");
