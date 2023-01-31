4dfd6352c94a96fe91bb0ca68b56d9e51e7f3a8c
Yahya Loutfi <soulang.dev@gmail.com>
2023-01-31 12:12:11 +0100

    to check in another visualizer

--- advanced_sort.c
+++ advanced_sort.c
@@ -6,7 +6,7 @@
 /*   By: yloutfi <yloutfi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/21 09:07:54 by yloutfi           #+#    #+#             */
-/*   Updated: 2023/01/30 20:24:31 by yloutfi          ###   ########.fr       */
+/*   Updated: 2023/01/31 11:59:05 by yloutfi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -149,30 +149,36 @@ t_stack		*find_cheapest(t_stack **stack)
 void	push_cheapest(t_stack **stack_a, t_stack **stack_b)
 {
 	t_stack	*cheap;
+	int		dest_pos;
+	int		cheap_pos;
 	
 	cheap = find_cheapest(stack_b);
-	while ((*stack_b)->rank != cheap->rank && (*stack_a)->rank != cheap->rank)
+	// while ((*stack_b)->rank != cheap->rank || (*stack_a)->rank != cheap->target_pos)
+	// {
+	// 	cheap_pos = get_pos(stack_b, cheap->rank);
+	// 	dest_pos = get_pos(stack_a, cheap->target_pos);
+	// 	if (cheap_pos <= ft_lstsize(*stack_b) / 2
+	// 		&& dest_pos <= ft_lstsize(*stack_a) / 2)
+	// 			double_inst("rr", stack_a, stack_b);
+	// 	else if (cheap_pos > ft_lstsize(*stack_b) / 2
+	// 		&& dest_pos > ft_lstsize(*stack_a) / 2)
+	// 			double_inst("rrr", stack_a, stack_b);
+	// }
+	while (*stack_b && (*stack_a)->rank != cheap->target_pos)
 	{
-		if ((*stack_b)->pos <= ft_lstsize(*stack_b) / 2
-			&& (*stack_a)->pos <= ft_lstsize(*stack_a) / 2)
-				double_inst("rr", stack_a, stack_b);
-		else if ((*stack_b)->pos > ft_lstsize(*stack_b) / 2
-			&& (*stack_a)->pos > ft_lstsize(*stack_a) / 2)
-				double_inst("rrr", stack_a, stack_b);
-	}
-	while ((*stack_a)->rank != cheap->rank)
-	{
-		if ((*stack_a)->pos <= ft_lstsize(*stack_a) / 2)
-				rotate(stack_a, "ra\n");
-		else if ((*stack_a)->pos > ft_lstsize(*stack_a) / 2)
-				rev_rotate(stack_a, "rra\n");
+		dest_pos = get_pos(stack_a, cheap->target_pos);
+		if (dest_pos <= ft_lstsize(*stack_a) / 2)
+			rotate(stack_a, "ra\n");
+		else
+			rev_rotate(stack_a, "rra\n");
 	}
 	while ((*stack_b)->rank != cheap->rank)
 	{
-		if ((*stack_b)->pos <= ft_lstsize(*stack_b) / 2)
-				rotate(stack_b, "rb\n");
-		else if ((*stack_b)->pos > ft_lstsize(*stack_b) / 2)
-				rev_rotate(stack_b, "rrb\n");
+		cheap_pos = get_pos(stack_b, cheap->rank);
+		if (cheap_pos <= ft_lstsize(*stack_b) / 2)
+			rotate(stack_b, "rb\n");
+		else
+			rev_rotate(stack_b, "rrb\n");
 	}
 	push(stack_a, stack_b, "pa\n");
 	return ;
@@ -183,27 +189,34 @@ void	advanced_sort(t_stack **stack_a, t_stack **stack_b)
 	stack_ranking(stack_a);
 	push_to_b(stack_a, stack_b);
 	simple_sort(stack_a, NULL);
-	// while (*stack_b)
-	// {
+	while (*stack_b)
+	{
 		set_pos(stack_a, stack_b);
 		set_target_pos(stack_a, stack_b);
 		set_cost_b(stack_b);
 		set_cost_a(stack_a, stack_b);
 		push_cheapest(stack_a, stack_b);
+	}
+	// while (is_sorted(stack_a, stack_b) == 0)
+	// {
+	// 	if (get_pos(stack_a, 1) <= ft_lstsize(*stack_a) / 2)
+	// 		rotate(stack_a, "ra\n");
+	// 	else
+	// 		rev_rotate(stack_a, "rra\n");
 	// }
-	printf("data\tpos\trank\tt_pos\tcost_a\tcost_b\n");
-	while (*stack_a)
-	{
-		printf("%d\t%d\t%d\t%d\t%d\t%d\n", (*stack_a)->data, (*stack_a)->pos, (*stack_a)->rank, (*stack_a)->target_pos, (*stack_a)->cost_stack_a, (*stack_a)->cost_stack_b);
-		(*stack_a) = (*stack_a)->next;
-	}
-	printf("--------------------\n");
-	while (*stack_b)
-	{
-		printf("%d\t%d\t%d\t%d\t%d\t%d\n", (*stack_b)->data, (*stack_b)->pos, (*stack_b)->rank, (*stack_b)->target_pos, (*stack_b)->cost_stack_a, (*stack_b)->cost_stack_b);
-		(*stack_b) = (*stack_b)->next;
-	}
-	printf("--------------------\n");
 	return ;
 }
+	// printf("data\tpos\trank\tt_pos\tcost_a\tcost_b\n");
+	// while (*stack_a)
+	// {
+	// 	printf("%d\t%d\t%d\t%d\t%d\t%d\n", (*stack_a)->data, (*stack_a)->pos, (*stack_a)->rank, (*stack_a)->target_pos, (*stack_a)->cost_stack_a, (*stack_a)->cost_stack_b);
+	// 	(*stack_a) = (*stack_a)->next;
+	// }
+	// printf("--------------------\n");
+	// while (*stack_b)
+	// {
+	// 	printf("%d\t%d\t%d\t%d\t%d\t%d\n", (*stack_b)->data, (*stack_b)->pos, (*stack_b)->rank, (*stack_b)->target_pos, (*stack_b)->cost_stack_a, (*stack_b)->cost_stack_b);
+	// 	(*stack_b) = (*stack_b)->next;
+	// }
+	// printf("--------------------\n");
 // , (*stack_a)->rank, (*stack_a)->target_pos
